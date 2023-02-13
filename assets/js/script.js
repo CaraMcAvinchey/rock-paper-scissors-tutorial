987203d53b821407a77afe81a23dc5dfee385a1b
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-02-13 10:49:46 +0000

    Add random computer choice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,12 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log("DOM Loaded")
 })
 
-let computerChoiceDisplay = document.getElementById('computer-choice')
-let userChoiceDisplay = document.getElementById('user-choice')
-let resultDisplay = document.getElementById('result')
-let possibleChoices = document.querySelectorAll('button')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
@@ -16,4 +17,19 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 
 }))
 
-function generateComputerChoice ()
+function generateComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 // can also use number possibleChoices.length
+    
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
