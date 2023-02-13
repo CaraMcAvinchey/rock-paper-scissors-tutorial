3451e45f432ee713d818b579db7cc3643ef269d7
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-02-13 10:34:55 +0000

    Add event listeners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,13 @@ let computerChoiceDisplay = document.getElementById('computer-choice')
 let userChoiceDisplay = document.getElementById('user-choice')
 let resultDisplay = document.getElementById('result')
 let possibleChoices = document.querySelectorAll('button')
+let userChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+
+}))
 
 function generateComputerChoice ()
